f11a09325dabc77a1d91e1383beeca6dd00d9b5b
Aryan-Gohel <aryan.gohel888@gmail.com>
2024-04-07 18:13:01 +0530

    Database Connection

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// To use this dot env as an impot as ES6 module rather than require syntex we have to do 
+// changes in the package json script 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
